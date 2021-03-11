441fd279e7873eac5ea089ccebbbe7ba93050091
2018dakotac <45215796+2018dakotac@users.noreply.github.com>
2021-03-10 20:40:47 -0800

    for board constants

---
+++ src/main/java/edu/sfu/os/chess/BoardConstants.java
@@ -0,0 +1,16 @@
+package edu.sfu.os.chess;
+
+public class BoardConstants {
+    // longs containing important bitboards for board manipulation
+    protected static final long FILE_A = 72340172838076673L;
+    protected static final long FILE_H = -9187201950435737472L;
+    protected static final long FILE_AB = 217020518514230019L;
+    protected static final long FILE_GH = -4557430888798830400L;
+    protected static final long RANK_1 = -72057594037927936L;
+    protected static final long RANK_4 = 1095216660480L;
+    protected static final long RANK_5 = 4278190080L;
+    protected static final long RANK_8 = 255L;
+    protected static final long CENTRE = 103481868288L;
+    protected static final long EXTENDED_CENTRE = 66229406269440L;
+
+}
